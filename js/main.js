fa59c880292681dce702f99c47f66bb536c605ed
yundig <157010828+yundig@users.noreply.github.com>
2025-05-08 22:11:52 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,28 @@
+const chooseColor = document.querySelectorAll('.choose-color__btn');
+const contentItem = document.querySelectorAll('.content-item');
+
+chooseColor.forEach(function (element) {
+  element.addEventListener('click', open)
+})
+
+function open(evt) {
+
+  const target = evt.currentTarget;
+  const button = target.dataset.button;
+  const contentActive = document.querySelectorAll(`.${button}`);
+
+  chooseColor.forEach(function (item) {
+    item.classList.remove('choose-color__btn--active')
+  })
+
+  target.classList.add('choose-color__btn--active')
+
+  contentItem.forEach(function (item) {
+    item.classList.remove('content-item__active')
+  });
+
+  contentActive.forEach(function (item) {
+    item.classList.add('content-item__active')
+  })
+
+}
